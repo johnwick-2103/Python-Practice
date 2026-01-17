af159eca4f3ace7f0e2afe37f74705ea86de4ccc
Aryan Doshi <aryandoshi21@gmail.com>
2026-01-17 21:33:02 +0530

    Time: 11 ms (56.45%), Space: 19.5 MB (13.31%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.py
+++ 1002-find-common-characters/1002-find-common-characters.py
@@ -1,23 +1,15 @@
 class Solution:
     def commonChars(self, words):
         result = []
-
-        # Count characters in the first word
         common = {}
         for ch in words[0]:
             common[ch] = common.get(ch, 0) + 1
-
-        # Compare with remaining words
         for word in words[1:]:
             temp = {}
             for ch in word:
                 temp[ch] = temp.get(ch, 0) + 1
-
-            # Keep minimum count
             for ch in common:
                 common[ch] = min(common[ch], temp.get(ch, 0))
-
-        # Add characters to result
         for ch in common:
             for _ in range(common[ch]):
                 result.append(ch)
