6dc9ae80bdc16e902fac09de72f539faffa8a30b
Aryan Doshi <aryandoshi21@gmail.com>
2026-02-08 23:16:10 +0530

    Time: 429 ms (29.88%), Space: 19.5 MB (32.76%) - LeetHub

--- 0454-4sum-ii/0454-4sum-ii.py
+++ 0454-4sum-ii/0454-4sum-ii.py
@@ -4,14 +4,12 @@ class Solution:
     def fourSumCount(self, nums1, nums2, nums3, nums4):
         countAB = Counter()
         
-        # All sums of nums1 and nums2
         for a in nums1:
             for b in nums2:
                 countAB[a + b] += 1
         
         result = 0
         
-        # Match with nums3 and nums4
         for c in nums3:
             for d in nums4:
                 result += countAB[-(c + d)]
