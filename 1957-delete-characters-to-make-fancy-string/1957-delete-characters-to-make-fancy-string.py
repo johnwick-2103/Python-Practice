51c716ebb5461e6cc5bd707398ff18007887c9a6
Aryan Doshi <aryandoshi21@gmail.com>
2026-05-15 23:48:23 +0530

    Time: 334 ms (21.22%), Space: 20.8 MB (88.49%) - LeetHub

---
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.py
@@ -0,0 +1,10 @@
+class Solution:
+    def makeFancyString(self, s: str) -> str:
+        res = ""
+
+        for c in s:
+            if len(res) >= 2 and res[-1] == c and res[-2] == c:
+                continue
+            res += c
+
+        return res
