7133a093bb81602c5a494b71bc03871f9341cf5c
Aryan Doshi <aryandoshi21@gmail.com>
2026-06-08 20:57:38 +0530

    Time: 0 ms (100%), Space: 19.2 MB (69.66%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findDifferentBinaryString(self, nums):
+        ans = ""
+
+        for i in range(len(nums)):
+            if nums[i][i] == '0':
+                ans += '1'
+            else:
+                ans += '0'
+
+        return ans
