857eb20f9c9299370327dfc375ba62c0d19f8692
Aryan Doshi <aryandoshi21@gmail.com>
2026-06-29 16:39:24 +0530

    Time: 0 ms (100%), Space: 19.2 MB (43.75%) - LeetHub

---
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.py
@@ -0,0 +1,12 @@
+class Solution:
+    def checkString(self, s: str) -> bool:
+        seen_b = False
+
+        for ch in s:
+            if ch == 'b':
+                seen_b = True
+
+            if ch == 'a' and seen_b:
+                return False
+
+        return True
